38093831f49506d58edb8a9f2499e3960048846d
hanandadras <hanan.aabid@gmail.com>
2021-10-20 11:48:25 -0700

    added generate password function

--- script.js
+++ script.js
@@ -2,16 +2,22 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.getElementById("generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+
+  var passwordText = document.getElementById("password");
 
   passwordText.value = password;
 
+
 }
 
+function generatePassword() {
+  console.log("click for password");
+  return "This would be the password"
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
