8fce865617594263ff2089d1b88e1b92c562d205
hanandadras <hanan.aabid@gmail.com>
2021-10-24 18:31:41 -0700

    updated README.md file

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # passwordgenerator
+This password generator code uses a window.alert to prompt the user to choose the length of password in the 8 to 128 range.
+if they cancel, the return command ends the function.
+the different variables numbers, lowercase,  uppercase, special characters and passcharacters are declared. the characters to be 
+the user is given the option to choose if they want any of the characters to be part of the password.
+the if statement specifies the code to be executed if the condition for the passcharacter's value is true.
--- script.js
+++ script.js
@@ -4,20 +4,77 @@
 // Get references to the #generate element
 var generateBtn = document.getElementById("generate");
 
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-
-  var passwordText = document.getElementById("password");
-
-  passwordText.value = password;
-
-
+var password = generatePassword();
+var passwordText = document.getElementById("password");
+passwordText.value = password;
 }
 
 function generatePassword() {
-  console.log("click for password");
-  return "This would be the password"
-}
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  var passwordLength = parseInt(window.prompt("Choose a number between 8 and 128"));
+  // console.log(typeof passwordLength);
+  if (passwordLength<8 || passwordLength>128) {
+    window.alert("This password is not valid");
+    return;
+  }
+
+  var numbers= ("0123456789")
+  var LowerCaseCharacters = ("abcdefghijklmnopqrstuvxyz");
+  var UppercaseCharacters = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  var specialCharacters = ("!@#$%^&*()_+");
+
+  var num = confirm ("do you want numbers in your password?");
+  var LCaseCharacters = confirm ("do you want LowerCaseCharacters in your password?");
+  var UcaseCharacters = confirm ("do you want UppercaseCharacters in your password?");
+  var SCharacters = confirm ("do you want specialCharacters in your password?");
+  var passCharacter = "";
+
+  if (num) {
+    passCharacter += numbers
+  }
+
+  if (LCaseCharacters) {
+    passCharacter += LowerCaseCharacters 
+  }
+
+  if (UcaseCharacters) {
+    passCharacter += UppercaseCharacters
+  }
+
+  if (SCharacters) {
+    passCharacter += specialCharacters
+  }
+
+  console.log(passCharacter)
+
+
+//functions for password generation
+  var finalPass = "";
+  for (let i=0; i < passwordLength; i++) {
+    finalPass += passCharacter.charAt(Math.floor(Math.random() * passCharacter.length))
+    console.log(finalPass)
+  }
+
+  return finalPass;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
